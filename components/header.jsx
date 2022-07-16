62a7d7a54e45e1253aa5517503473679c4037c67
Pranay Surve <pranaysurve75@gmail.com>
2022-07-16 23:41:30 +0530

    home page used ssr

--- components/header.jsx
+++ components/header.jsx
@@ -2,7 +2,7 @@ import Link from 'next/link'
 import Image from 'next/image'
 import { useRouter } from 'next/router'
 
-const Header = () => {
+const Header = (props) => {
   const router = useRouter();
   const currentRoute = router.pathname;
 //  alert(currentRoute);
@@ -51,5 +51,10 @@ const Header = () => {
       } `}</style>
     </nav>
   )
+}
+export async function getStaticProps() {
+  return {
+    props: {},
+  }
 }
 export default Header;
